5e1b7fe97c394aa77dfd244c616714e8e4441bbc
harikrishnan9944 <hk9917732@gmal.com>
2025-03-26 12:41:59 +0000

    hello

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen w-full text-5xl text-green-500 flex justify-center items-center">
-      Hello World
+      Hello World E-commerce
     </div>
   );
 }
