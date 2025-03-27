9d7f7ed3e289215c563d4a7d9fb42eda616c846a
harikrishnan9944 <hk9917732@gmal.com>
2025-03-27 13:01:20 +0000

    ui updates

---
+++ my-app/src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+
+
+export default function RegisterPage(){
+
+    return (
+      <div className="h-screen w-full flex justify-center items-center bg-gray-50 ">
+          <div className="bg-white w-[20rem] h-[20rem] rounded-b-xl  shadow-2xl flex justify-center items-center">
+         {/* <input type="text" className=""/> */}
+          </div>
+      </div>
+    )
+}
